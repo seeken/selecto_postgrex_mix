f228a9d6f0e4bdf00d8ca922b33498cd27a2d181
Chris Rohlfs <seeken@gmail.com>
2026-03-12 09:38:19 -0600

    chore: raise minimum Elixir version to 1.18

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SelectoPostgrexMix.MixProject do
     [
       app: :selecto_postgrex_mix,
       version: "0.1.2",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
